b5e6644a0eed9bd2b73929f791bead5d3817f429
Xavier Lange <xrlange@gmail.com>
2011-09-24 19:33:13 -0700

    Updated framing unit test

--- test/framing.c
+++ test/framing.c
@@ -33,9 +33,14 @@ void test_frame_small_with_mask(size_t *size, uint8_t **frame){
 
 	mongrel2_ws_frame_set_fin(*size,*frame);
 	mongrel2_ws_frame_set_opcode(*size,*frame,OP_TEXT);
+	uint8_t data[4];
+	data[0] = 0xFF;
+	data[1] = 0xEE;
+	data[2] = 0xAA;
+	data[3] = 0xDD;
+
+	mongrel2_ws_frame_set_payload(*size,*frame,4,data);
 	mongrel2_ws_frame_set_mask(*size,*frame,0xFFEEAADD);
-	char* data = "ASDF";
-	mongrel2_ws_frame_set_payload(*size,*frame,4,(uint8_t*)data);
 }
 
 
@@ -47,14 +52,14 @@ void test_frame_medium_no_mask(size_t *size, uint8_t **frame){
 	mongrel2_ws_frame_set_opcode(*size,*frame,OP_BIN);
 }
 
-// void test_frame_medium_with_mask(size_t *size, uint8_t **frame){
-// 	int retval = mongrel2_ws_frame_create(0,2244,size,frame);
-// 	assert(retval == 0);
+void test_frame_medium_with_mask(size_t *size, uint8_t **frame){
+	int retval = mongrel2_ws_frame_create(1,2244,size,frame);
+	assert(retval == 0);
 
-// 	mongrel2_ws_frame_set_fin(*size,*frame);
-// 	mongrel2_ws_frame_set_opcode(*size,*frame,OP_BIN);
-// 	mongrel2_ws_frame_set_mask(*size,*frame,0xDDEEAADD);
-// }
+	mongrel2_ws_frame_set_fin(*size,*frame);
+	mongrel2_ws_frame_set_opcode(*size,*frame,OP_BIN);
+	mongrel2_ws_frame_set_mask(*size,*frame,0xFFFFFFFF);
+}
 
 void test_frame_large_no_mask(size_t *size, uint8_t **frame){
 	int retval = mongrel2_ws_frame_create(0,66000,size,frame);
@@ -69,6 +74,8 @@ int main(int argc, char** args){
 	uint8_t *frame;
 	size_t size;
 
+	printf("==== NO MASK ====\n");
+
 	test_frame_small_no_mask(&size,&frame);
 	mongrel2_ws_frame_debug(size,frame);
 	free(frame);
@@ -81,13 +88,15 @@ int main(int argc, char** args){
 	mongrel2_ws_frame_debug(size,frame);
 	free(frame);
 
+	printf("======== W/ MASK ========\n");
+
 	test_frame_small_with_mask(&size,&frame);
 	mongrel2_ws_frame_debug(size,frame);
 	free(frame);
 
-	// test_frame_medium_with_mask(&size,&frame);
-	// mongrel2_debug_frame(size,frame);
-	// free(frame);
+	test_frame_medium_with_mask(&size,&frame);
+	mongrel2_ws_frame_debug(size,frame);
+	free(frame);
 
 	return 0;
 }
