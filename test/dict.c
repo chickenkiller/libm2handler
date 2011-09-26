cba727e6ecf5e61b84a7765d78597a6c1cd20eb1
Xavier Lange <xrlange@gmail.com>
2011-09-26 12:01:15 -0700

    Working dictionary example

--- test/dict.c
+++ test/dict.c
@@ -7,65 +7,67 @@
 #include "adt/dict.h"
 
 typedef struct key_t {
-	int key;
+    int key;
 } key;
 
 typedef struct value_t {
-	char *string;
+    char *string;
 } value;
 
 int compare_request(const void *key1_void, const void *key2_void){
-	key *key1 = (key*)key1_void;
-	key *key2 = (key*)key2_void;
-	printf("Comparing %d and %d\n",key1->key,key2->key);
-	if(key1->key > key2->key){
-		return 1;
-	} else if(key1->key < key2->key){
-		return -1;
-	} else{
-		return 0;
-	}
+    key *key1 = (key*)key1_void;
+    key *key2 = (key*)key2_void;
+    printf("Comparing %d and %d\n",key1->key,key2->key);
+    if(key1->key > key2->key){
+        return 1;
+    } else if(key1->key < key2->key){
+        return -1;
+    } else{
+        return 0;
+    }
 }
 
 static dnode_t *alloc_dict(void *notused) {
-	printf("Asked to allocate\n");
+    printf("Asked to allocate\n");
     return (dnode_t *)calloc(sizeof(dnode_t), 1);
 }
 
 static void free_dict(dnode_t *node, void *notused) {
-	printf("Asked to free a value\n");
+    printf("Asked to free a value\n");
+    key *keyptr = (key*)dnode_getkey(node);
+    value *valptr = (value*)dnode_get(node);
+    free(keyptr);
+    free(valptr);
     free(node);
 }
 
 int main(int argc, char **args){
-	dict_t* dict = dict_create(DICTCOUNT_T_MAX, compare_request);
-	dict_set_allocator(dict, alloc_dict, free_dict, NULL);
+    dict_t* dict = dict_create(DICTCOUNT_T_MAX, compare_request);
+    dict_set_allocator(dict, alloc_dict, free_dict, NULL);
 
-	key *key1 = calloc(1,sizeof(key));
-	key1->key = 10;
-	key *key2 = calloc(1,sizeof(key));
-	key2->key = 5;
+    key *key1 = calloc(1,sizeof(key));
+    key1->key = 10;
+    key *key2 = calloc(1,sizeof(key));
+    key2->key = 5;
 
-	value *val1 = calloc(1,sizeof(value));
-	val1->string = "unsafe memory!";
-	value *val2 = calloc(1,sizeof(value));
-	val2->string = "unsafe memor!";
+    value *val1 = calloc(1,sizeof(value));
+    val1->string = "unsafe memory!";
+    value *val2 = calloc(1,sizeof(value));
+    val2->string = "unsafe memor!";
 
-	printf("Starting test 1\n");
-	dnode_t *node1 = dnode_create(key1);
-	dict_alloc_insert(dict,node1,val1);
-	assert(dict_count(dict) == 1);
-	assert(dict_contains(dict,node1) != 0);
-	printf("Ensuring node was added\n");
-	dnode_t *temp1 = dict_lookup(dict,key1);
-	assert(temp1 != NULL);
+    assert(dict_alloc_insert(dict,key1,val1) == 1);
+    assert(dict_count(dict) == 1);
+    dnode_t *temp1 = dict_lookup(dict,key1);
+    assert(temp1 != NULL);
+    assert(((key*)dnode_getkey(temp1))->key == 10);
+    assert(((value*)dnode_get(temp1))->string != NULL);
 
-	// dnode_t *node2 = dnode_create(key2);
-	// dict_insert(dict,node2,val2);
-	// assert(dict_contains(dict,node2));
-	// assert(dict_count(dict) == 2);
+    assert(dict_alloc_insert(dict,key2,val2) == 1);
+    assert(dict_count(dict) == 2);
+    dnode_t *temp2 = dict_lookup(dict,key2);
+    assert(temp2 != NULL);
 
-	dict_free_nodes(dict);
+    dict_free_nodes(dict);
     dict_destroy(dict);
-	return 0;
+    return 0;
 }
